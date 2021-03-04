fb8724a5496a6ecdd4d4c7ef2c76a3282554fb65
novastosha <galliigotu@gmail.com>
2021-03-04 19:01:15 +0100

    Created CustomCommandListener for later use...

---
+++ src/dev/nova/menus/commands/CustomCommandListener.java
@@ -0,0 +1,14 @@
+package dev.nova.menus.commands;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class CustomCommandListener implements Listener {
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event){
+
+    }
+
+}
