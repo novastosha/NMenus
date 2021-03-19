154736843b57eaf70ad49e997d261e0294e52a18
novastosha <galliigotu@gmail.com>
2021-03-19 08:02:31 +0100

    Created base code, will be coded later when i add bungeecord support.

---
+++ src/dev/nova/menus/menu/actions/ConnectToServer.java
@@ -0,0 +1,15 @@
+/**
+ * REQUIRES BUNGEECORD
+ */
+package dev.nova.menus.menu.actions;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+
+public class ConnectToServer extends Action{
+
+    @Override
+    public void executeAction(Player player, ClickType clickType) {
+
+    }
+}
