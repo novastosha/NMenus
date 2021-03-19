198574536cb13544b3b5176ec2a231588e162082
novastosha <galliigotu@gmail.com>
2021-03-19 08:03:18 +0100

    Created debug message sender.

---
+++ src/dev/nova/menus/utils/DebugMessenger.java
@@ -0,0 +1,19 @@
+package dev.nova.menus.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+public class DebugMessenger
+{
+    private final Plugin plugin;
+
+    public DebugMessenger(Plugin menus){
+        this.plugin = menus;
+    }
+
+    public void sendMLM(String message) {
+        if(plugin.getConfig().getBoolean("more-loading-message")){
+            Bukkit.getConsoleSender().sendMessage("§7[§eNMenus§7] "+message);
+        }
+    }
+}
