680c77c93bc04fd6f04e8eec738011b5f63dcf6a
novastosha <galliigotu@gmail.com>
2021-04-02 11:33:15 +0100

    Created NBase64 to handle base64 codes.

---
+++ src/dev/nova/menus/utils/head/NBase64.java
@@ -0,0 +1,14 @@
+package dev.nova.menus.utils.head;
+
+public class NBase64 {
+
+    private final String strBase64;
+
+    public NBase64(String strBase64){
+        this.strBase64 = strBase64;
+    }
+
+    public String getStrBase64() {
+        return strBase64;
+    }
+}
