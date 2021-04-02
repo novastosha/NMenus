d97c755ad9f562cdb9e1e60a6bdea24d2a8b7232
novastosha <galliigotu@gmail.com>
2021-04-02 11:27:49 +0100

    Added action registration, along with action id.

--- src/dev/nova/menus/menu/actions/Action.java
+++ src/dev/nova/menus/menu/actions/Action.java
@@ -3,8 +3,25 @@ package dev.nova.menus.menu.actions;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 
+import java.util.ArrayList;
+
 public abstract class Action {
+
+    public static final ArrayList<Action> REGISTERED_ACTIONS = new ArrayList<>();
+    private final String id;
 
     public abstract void executeAction(Player player, ClickType clickType);
 
+    public Action(String id){
+        this.id = id;
+        register();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    protected void register(){
+        REGISTERED_ACTIONS.add(this);
+    }
 }
