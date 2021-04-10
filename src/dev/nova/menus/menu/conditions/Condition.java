cdc9c87003884505862dc540f06cfe6a8db0490e
novastosha <galliigotu@gmail.com>
2021-04-10 13:00:50 +0100

    Created this class to handle conditions.

--- src/dev/nova/menus/menu/conditions/Condition.java
+++ src/dev/nova/menus/menu/conditions/Condition.java
@@ -1,7 +1,11 @@
 package dev.nova.menus.menu.conditions;
 
+import dev.nova.menus.menu.Menu;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+
 public abstract class Condition {
 
-
+    public abstract Object check(Player player, ClickType clickType, Menu menu);
 
 }
